1bbd2befd65c7fe36bc68bdc8041848c3155b7d8
Cassan0va27 <pushkarmahajan4@gmail.com>
2023-04-25 18:10:13 -0400

    Add cors in backend

--- movie_frontend/src/App.js
+++ movie_frontend/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [movieData, setMovieData] = useState("");
+  useEffect(() => {
+    fetch("http://127.0.0.1:8080/api/v1/movies/getMovie/tt3915174")
+      .then((response) => response.json())
+      .then((data) => setMovieData(data));
+  }, []);
   return (
     <div className="App">
-      <p>Hello World</p>
+      <p>{console.log(movieData)}</p>
     </div>
   );
 }
--- movies/src/main/java/com/pushkar/movies/Controller/Rest_Controller.java
+++ movies/src/main/java/com/pushkar/movies/Controller/Rest_Controller.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 
 
@@ -26,5 +27,4 @@ public class Rest_Controller {
     private ResponseEntity<Movie> getMovieByID(@PathVariable String id){
         return new ResponseEntity<Movie>(movieService.getMovieByimdbId(id), HttpStatus.OK);
     }
-
 }
